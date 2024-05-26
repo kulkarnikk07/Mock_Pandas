ad4bab6bcedd2eb91495d7ecfe3df8f51a65a12f
Kedar Kulkarni <kulkarnikk07@gmail.com>
2024-05-26 11:20:48 -0700

    completed Mock pandas 5/26

--- Mock_Pandas.py
+++ Mock_Pandas.py
@@ -17,9 +17,25 @@ def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
     df = pd.DataFrame(df.max(), columns = ['num'])
     return df
 
-# 3 Problem 3 :Sales Analysis III (Mock Problem) (https://leetcode.com/problems/sales-analysis-iii/)
+# 3 Problem 3 :Sales Analysis III (Mock Problem) (https://leetcode.com/problems/sales-analysis-iii/) 5/26/2024
 
+import pandas as pd
 
+def sales_analysis(product: pd.DataFrame, sales: pd.DataFrame) -> pd.DataFrame:
+    start_date = pd.to_datetime('2019-01-01')
+    end_date = pd.to_datetime('2019-03-31')
 
-# 4 Problem 4 : Market Analysis I (Mock Problem) ( https://leetcode.com/problems/market-analysis-i/) 
+    df = sales.groupby(['product_id']).filter(lambda x: min(x['sale_date']) >= start_date and max(x['sale_date'])<=end_date)
+    df = df.drop_duplicates(subset = 'product_id')
+    result = df.merge(product, on = 'product_id', how = 'left')
+    return result[['product_id','product_name']]
 
+# 4 Problem 4 : Market Analysis I (Mock Problem) ( https://leetcode.com/problems/market-analysis-i/)  5/26/2024
+
+import pandas as pd
+
+def market_analysis(users: pd.DataFrame, orders: pd.DataFrame, items: pd.DataFrame) -> pd.DataFrame:
+    df = orders[(orders['order_date'] >= '2019-01-01') & (orders['order_date'] <= '2019-12-31')]
+    df = df.groupby('buyer_id')['order_id'].size().reset_index(name ='orders_in_2019')
+    result = users.merge(df, left_on ='user_id', right_on ='buyer_id', how ='left').fillna(0)
+    return result[['user_id','join_date','orders_in_2019']].rename(columns = {'user_id':'buyer_id'})
